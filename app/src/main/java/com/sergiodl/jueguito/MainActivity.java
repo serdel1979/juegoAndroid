f937b679288152b8ae6894842f842cbcbfb47636
sergio <sdlbsso@gmail.com>
2021-01-05 00:52:04 -0300

    juego para android

---
+++ app/src/main/java/com/sergiodl/jueguito/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sergiodl.jueguito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
